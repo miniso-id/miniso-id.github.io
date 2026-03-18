5df8ec604b1124eb705f746178225d38798d6195
miniso-id <113236467+miniso-id@users.noreply.github.com>
2026-03-18 10:50:56 +0700

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mms-x-v1.3.5'; // Pastikan versi ini sama dengan di HTML
+const CACHE_NAME = 'mms-x-v1.3.6'; // Pastikan versi ini sama dengan di HTML
 const OFFLINE_URL = './offline.html'; // Tentukan URL offline
 const ASSETS_TO_CACHE = [
   './',

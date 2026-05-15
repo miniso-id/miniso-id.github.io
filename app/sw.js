d21ce391e22bd4017fb19c4af76f0a2c20ae4c01
miniso-id <113236467+miniso-id@users.noreply.github.com>
2026-05-15 10:29:24 +0700

    Update CACHE_NAME version to 3.0.0.7

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mms-x-v3.0.0.6'; // Pastikan versi ini sama dengan di HTML
+const CACHE_NAME = 'mms-x-v3.0.0.7'; // Pastikan versi ini sama dengan di HTML
 const OFFLINE_URL = './offline.html'; // Tentukan URL offline
 const ASSETS_TO_CACHE = [
   './',
